078bde2cdc98fc594da277f331969589844c076e
adiarora7 <96757084+adiarora7@users.noreply.github.com>
2023-11-22 22:43:06 -0800

    Added Visualization
    Added hand landmarks visualization and gesture is displayed on screen.

--- main.py
+++ main.py
@@ -10,14 +10,30 @@ GestureRecognizerOptions = mp.tasks.vision.GestureRecognizerOptions
 GestureRecognizerResult = mp.tasks.vision.GestureRecognizerResult
 VisionRunningMode = mp.tasks.vision.RunningMode
 
-def print_result(result: GestureRecognizerResult, output_image: mp.Image, timestamp_ms: int):
-    print('Gesture recognition result:', result)
 
+current_gesture = ""
+
+def print_result(result: GestureRecognizerResult, output_image: mp.Image, timestamp_ms: int):
+    print("Gesture Recognizer Results:", result)
+
+    global current_gesture
+
+    if result.gestures:
+        top_gesture = result.gestures[0][0]
+        current_gesture = f"{top_gesture.category_name} ({top_gesture.score:.2f})"
+    return result
 
 
 def main():
-    # Initialize the camera (0 is the default camera)
+    global current_gesture
+
+    # Initialize the camera (0 is iPhone camera)
     cap = cv2.VideoCapture(1)
+
+    # Drawing utilities
+    mp_hands = mp.solutions.hands
+    hands = mp_hands.Hands()
+    mp_drawing = mp.solutions.drawing_utils
 
     # Check if the camera opened successfully
     if not cap.isOpened():
@@ -43,6 +59,18 @@ def main():
                     print("Can't receive frame (stream end?). Exiting ...")
                     break
 
+                # Mirrored display
+                frame = cv2.flip(frame, 1)
+
+                # Convert frame for hand tracking
+                image_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+                hand_results = hands.process(image_rgb)
+
+                # Draw hand landmarks
+                if hand_results.multi_hand_landmarks:
+                    for hand_landmarks in hand_results.multi_hand_landmarks:
+                        mp_drawing.draw_landmarks(frame, hand_landmarks, mp_hands.HAND_CONNECTIONS)
+
                 # Convert frame to MediaPipe Image
                 mp_frame = mp.Image(image_format=mp.ImageFormat.SRGB, data=frame)
                 
@@ -52,6 +80,9 @@ def main():
 
                 # Perform gesture recognition
                 recognizer.recognize_async(mp_frame, timestamp_ms)
+                
+                # Draw the gesture name on the frame
+                cv2.putText(frame, current_gesture, (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 128, 0), 2, cv2.LINE_AA)
 
                 # Display the resulting frame
                 cv2.imshow('Camera Feed', frame)
@@ -59,10 +90,12 @@ def main():
                 # Break the loop when 'q' is pressed
                 if cv2.waitKey(1) == ord('q'):
                     break
+
         finally:
-            # When everything is done, release the capture
+            # When everything is done, release the capture and hands
             cap.release()
             cv2.destroyAllWindows()
+            hands.close()
 
 
 
